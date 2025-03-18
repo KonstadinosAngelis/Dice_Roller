a78ab0047c69b5d06fbf885a32698d55cb3dffc5
KonstadinosAngelis <angeliskonstadinos@gmail.com>
2025-03-17 23:26:28 -0700

    Added a text bar for entry with a regex expression that stops it from being sent if it doesn't fit the format which I put into a .env file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,47 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [result, setResult] = useState(0);
-  const [sides, setSides] = useState(20);
-  const [amount, setAmount] = useState(3);
+  //Import REGEX 
+  const regexPattern = import.meta.env.VITE_DICE_REGEX;
+  const diceRegex = new RegExp(regexPattern);
 
-  const rollDice = () => {
-    let total = 0;
-    //Rolls the number of dice set by the user
-    for (let i = amount; i > 0; i--) {
-      let rolledNum = Math.floor(Math.random() * sides) + 1;
-      total += rolledNum;      
+  //Total Calculated
+  const [result, setResult] = useState(0);
+
+  const [amountOfDice, setAmountOfDice] = useState(1);
+  const [amountOfSides, setAmountOfSides] = useState(20);
+
+  //Text user input
+  const [diceRollInput, setDiceRollInput] = useState("");
+
+  const rollDice = (userInput) => {
+    let runningTotal = 0;
+
+    let amountOfDice = userInput.split("d")[0];
+    let amountOfSides = userInput.split("d")[1];
+
+    //Rolls the number of dice set by the user.
+    for (let i = amountOfDice; i > 0; i--) {
+        let rolledNum = Math.floor(Math.random() * amountOfSides) + 1;
+        runningTotal += rolledNum;
     }
-    setResult(total);
+
+    setAmountOfDice(userInput.split("d")[0]);
+    setAmountOfSides(userInput.split("d")[1]);
+    setResult(runningTotal);
   }
+
+  // DEPRICATED VERSION OF THE DICE ROLLER
+  // const rollDice = () => {
+  //   let total = 0;
+  //   //Rolls the number of dice set by the user
+  //   for (let i = amount; i > 0; i--) {
+  //     let rolledNum = Math.floor(Math.random() * sides) + 1;
+  //     total += rolledNum;      
+  //   }
+  //   setResult(total);
+  // }
 
   return (
     <>
@@ -24,17 +51,40 @@ function App() {
       </header>
       <section className="card">
           <h2>Number Rolled: {result}!</h2>
-          <h3>Rolling a D{sides}</h3>
-          <input type='text'>Test</input>
+          <p>Type a number of dice to roll and the number of sides each has using the format (2d6, or 1d20)</p>
+          <input
+            type="text"
+            value={diceRollInput}
+            onChange={(e) => setDiceRollInput(e.target.value)}
+            placeholder="Enter in format 2d6 or 1d20"
+            onKeyDown={(e) => {
+              if(e.key === "Enter") {
+                console.log(regexPattern)
+                if(diceRegex.test(e.target.value)) {
+                  setAmountOfDice(e.target.value.split("d")[0]);
+                  setAmountOfSides(e.target.value.split("d")[1]);
+                  rollDice(e.target.value)
+                }
+                else {
+                  alert("Please enter a valid input (100d100 is the max!)")
+                }
+              }
+            }}
+            />
+            <h3>Rolling a: D{amountOfSides} {amountOfDice} time(s)</h3>
             <section className="diceSelection">
-              <button onClick={() => setSides(4)}>D4</button>
-              <button onClick={() => setSides(6)}>D6</button>
-              <button onClick={() => setSides(8)}>D8</button>
-              <button onClick={() => setSides(10)}>D10</button>
-              <button onClick={() => setSides(12)}>D12</button>
-              <button onClick={() => setSides(20)}>D20</button>
+              <div>
+                <button onClick={() => setAmountOfSides(4)}>D4</button>
+                <button onClick={() => setAmountOfSides(6)}>D6</button>
+                <button onClick={() => setAmountOfSides(8)}>D8</button>
+              </div>
+              <div>
+                <button onClick={() => setAmountOfSides(10)}>D10</button>
+                <button onClick={() => setAmountOfSides(12)}>D12</button>
+                <button onClick={() => setAmountOfSides(20)}>D20</button>
+              </div>
             </section>
-          <button onClick={() => rollDice(sides)}>
+          <button onClick={() => rollDice(diceRollInput)}>
             Roll!
           </button>
       </section>
